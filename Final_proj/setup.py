3e04b221d5401f62e7597f001a281bf83163661c
Itayrub <rubanenko.itay@gmail.com>
2026-03-02 13:55:31 +0200

    final_proj_folder

---
+++ Final_proj/setup.py
@@ -0,0 +1,7 @@
+from setuptools import Extension, setup
+
+module = Extension("capi_demo1", sources=['kmeans.c','genmodule.c'])
+setup(name='capi_demo1',
+     version='1.0',
+     description='Python wrapper for custom C extension',
+     ext_modules=[module])
