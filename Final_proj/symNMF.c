bfb3da182035c9389b80f2b6bf14b6e59f379ecd
ItamarACGN <Itamarnir10@gmail.com>
2026-03-02 16:16:05 +0200

    bug fixing

--- Final_proj/symNMF.c
+++ Final_proj/symNMF.c
@@ -4,34 +4,45 @@
 #include <math.h>
 
 Matrix* sym(struct Vector *v, int n, int d){
+    int i,j;
     Matrix *result = matrix_create(n, n);
     if (result == NULL) return NULL;
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for ( i = 0; i < n; i++) {
+        for ( j = 0; j < n; j++) {
             double dist = calculateDistance(&v[i], &v[j], d);
-            matrix_set(result, i, j, exp(-dist/2));
+            if(i!=j) {
+                matrix_set(result, i, j, exp(-dist/2));
+            } else {
+                matrix_set(result, i, j, 0.0);
+            }
         }
     }
     return result;
 }
 
 Matrix* ddg(struct Vector *v, int n, int d){
+    int i,j;
     Matrix *result = matrix_create(n, n);
-    if (result == NULL) return NULL;
-    
-    for (int i = 0; i < n; i++) {
+    Matrix *symMatrix = sym(v, n, d);
+    if (result == NULL || symMatrix == NULL) {
+        matrix_free(result);
+        matrix_free(symMatrix);
+        return NULL;
+    }
+    for ( i = 0; i < n; i++) {
         double sum = 0.0;
-        for (int j = 0; j < n; j++) {
-            double dist = calculateDistance(&v[i], &v[j], d);
-            sum += dist;
+        for ( j = 0; j < n; j++) {
+            sum += matrix_get(symMatrix, i, j);
         }
         matrix_set(result, i, i, sum);
     }
+    matrix_free(symMatrix);
     return result;
 }
 
 Matrix* norm(struct Vector *v, int n, int d){
+    int i;
     Matrix *symMatrix = sym(v, n, d);
     if (symMatrix == NULL) return NULL;
     
@@ -40,7 +51,7 @@ Matrix* norm(struct Vector *v, int n, int d){
         matrix_free(symMatrix);
         return NULL;
     }
-    for(int i = 0; i < n; i++){
+    for(i = 0; i < n; i++){
         ddgMatrix->data[i * n + i] = 1.0 / sqrt(ddgMatrix->data[i * n + i]);
     }
     Matrix *temp = matrix_multiply(ddgMatrix, symMatrix);
@@ -64,16 +75,6 @@ Matrix* norm(struct Vector *v, int n, int d){
 
     return normMatrix;
 }
-
-
-int main(int argc, char *argv[]) {
-    if(argc != 2) {
-        fprintf(stderr, "An error has occurred\n");
-        exit(1);
-    }
-
-}# include "symnmf.h"
-
 
 Matrix * update_H (Matrix * curr_H, Matrix * W){
     Matrix * next_H = matrix_create(curr_H -> rows, curr_H -> cols);
@@ -107,5 +108,11 @@ Matrix * optimize_H(Matrix * H, Matrix * W, int max_iters, double epsilon){
     return curr_H;
 }
 
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        fprintf(stderr, "An error has occurred\n");
+        exit(1);
+    }
 
+}
 
