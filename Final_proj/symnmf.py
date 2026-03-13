19103f9f81e77a82c218d3833755876a24641ff6
itayrub <rubanenko.itay@gmail.com>
2026-03-13 16:25:47 +0000

    started analysis

--- Final_proj/symnmf.py
+++ Final_proj/symnmf.py
@@ -108,13 +108,16 @@ def execute_goal(data, N, k_val, d, goal, max_iter = MAX_ITER, epsilon = EPSILON
     return np.array(result)
 
 
-if __name__ == "__main__":
+def main():
     input_data = read_input()
     if input_data is not None:
         #print( input_data)
         data, N, k_val, d, goal = input_data
         result = execute_goal(data.tolist(), N, k_val, d, goal)
-        if result is not None:
-            #TODO make sure the matrix is not transposed!!
-            np.savetxt(stdout, result, fmt="%.4f", delimiter=", ")#prints the resault
-
+        return res
+        
+if __name__ == "__main__":
+    res = main()
+    if res is not None:
+        #TODO make sure the matrix is not transposed!!
+        np.savetxt(stdout, res, fmt="%.4f", delimiter=", ")#prints the resault
