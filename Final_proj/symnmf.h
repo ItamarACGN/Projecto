5e621a352a8fff548b189363e4c8eecb56b29b2d
Itayrub <rubanenko.itay@gmail.com>
2026-03-14 15:29:56 +0200

    some fixes and recompiled

--- Final_proj/symnmf.h
+++ Final_proj/symnmf.h
@@ -38,7 +38,7 @@ Matrix* ddg(struct Vector *v, int n, int d); /* Compute the degree matrix */
 Matrix* sym(struct Vector *v, int n, int d); /* Compute the similarity matrix*/
 Matrix* norm(struct Vector *v, int n, int d); /* Compute the normalized similarity matrix */
 Matrix* optimize_H(Matrix * H, Matrix * W, int max_iters, double epsilon); /* Optimize H matrix using multiplicative update rules */
-void error_and_exit(); /*prints error messege and quirs - helper*/
+void error_and_exit(void); /*prints error messege and quirs - helper*/
 Matrix* diagonal_multiply_matrix(const Matrix *Diag, const Matrix *A);
 Matrix* matrix_multiply_diagonal(const Matrix *A, const Matrix *Diag);
 #endif /* CAP_H_ */
